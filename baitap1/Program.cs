8d89e7feeea7b1da711c369d47d73b8e2c96359d
ADMIN <ADMIN@PC329>
2024-01-03 08:01:10 +0700

    Add project files.

---
+++ baitap1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap1
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle()
+        {
+            length = 1.0;
+            width = 1.0;
+
+        }
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+
+        }
+        public void setLength(double length)
+        {
+            this.length = length;
+
+        }
+        public double getLength()
+        {
+            return length;
+        }
+        public void setWidth(double width)
+        {
+            this.width = width;
+        }
+        public double getWidth()
+        {
+            return width;
+        }
+        public double FindArea()
+        {
+            return length * width;
+        }
+        public double FindPerimeter()
+        {
+            return (length + width) * 2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1;
+            Rectangle r2;
+
+            r1 = new Rectangle();
+            Console.Write("Cho biet chieu dai: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Cho biet chieu rong: ");
+            double width = double.Parse(Console.ReadLine());
+
+            r2 = new Rectangle(length, width);
+
+            Console.WriteLine("--------Thong tin hinh chu nhat r1-------");
+            Console.WriteLine("Chieu dai: {0}\n Chieu rong: {1}\n Dien tich: {2}\n Chu vi: {3}",
+                r1.getLength(), r1.getWidth(), r1.FindArea(), r1.FindPerimeter());
+
+            Console.WriteLine("--------Thong tin hinh chu nhat r2-------");
+            Console.WriteLine("Chieu dai: {0}\n Chieu rong: {1}\n Dien tich: {2}\n Chu vi: {3}",
+                r2.getLength(), r2.getWidth(), r2.FindArea(), r2.FindPerimeter());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ baitap2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap2
+{
+    class Account
+    {
+        private string id;
+        private string name;
+        private int balance;
+        public Account(string id, string name, int balance)
+        {
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+        public string getID()
+        {
+            return id;
+        }
+        public string getname()
+        {
+            return name;
+        }
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void credit(int amount)
+        {
+            if (amount > 0)
+                balance += amount;
+        }
+        public void debit(int amount)
+        {
+            if (amount <= balance)
+                balance -= amount;
+            else
+                Console.WriteLine("So tien vuot qua so du. Rut tien khong thanh cong");
+
+        }
+        public void tranferTo(Account acc, int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                acc.balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("So tien vuot so du. Chuyen khoan khong thanh cong");
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account my = new Account("900111", "Le Van Ti", 600);
+            Account myFriend = new Account("900112", "Be Le Van Dat", 30);
+            Console.WriteLine("------Thong tin 2 tai khoan ban dau----");
+            Console.WriteLine("My Account:[ id;{0}, name: {1}, balance: {2}",
+                my.getID(), my.getname(), my.getBalance());
+            Console.WriteLine("My Friend Account:[ id;{0}, name: {1}, balance: {2}",
+                myFriend.getID(), myFriend.getname(), myFriend.getBalance());
+
+            //rut tien
+
+            my.debit(200);
+            my.tranferTo(myFriend, 450);
+
+            //ket qua sau khi thuc hien mot so hanh vi(nap, rut, chuyen khoan)
+
+            Console.WriteLine("---Thong tin 2 tai khoan sau khi thuc hien giao dich---");
+            Console.WriteLine("My Account:[ id;{0}, name: {1}, balance: {2}",
+                 my.getID(), my.getname(), my.getBalance());
+            Console.WriteLine("My Friend Account:[ id;{0}, name: {1}, balance: {2}",
+               myFriend.getID(), myFriend.getname(), myFriend.getBalance());
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
+
