d1856227819533e174a3f9d6f4bc34a89d5a69da
hluan1629 <hluan1629@gmail.com>
2024-01-05 10:14:49 +0700

    baitap5

---
+++ baitap5/CD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap5
+{
+    class CD
+    {
+        private int macd;
+        private string tuacd;
+        private string casi;
+        private int sobaihat;
+        private int donggia;
+
+        public CD()
+        {
+        }
+        public CD(int macd)
+        {
+            this.macd = macd;
+        }
+        public CD(int macd, string tuacd, string casi, int sobaihat, int donggia)
+        {
+            this.macd = macd;
+            this.tuacd = tuacd;
+            this.casi = casi;
+            this.sobaihat = sobaihat;
+            this.donggia = donggia;
+
+        }
+        public int MACD
+        {
+            set { this.macd = value;  }
+            get { return macd; }
+        }
+        public string TuaCD
+        {
+            set { this.tuacd = value;  }
+            get { return tuacd; }
+        }
+        public string Casi
+        {
+            set { this.casi = value; }
+            get { return casi; }
+        }
+        public int SoBaiHat
+        {
+            set { this.sobaihat = value; }
+            get { return sobaihat; }
+        }
+        public int DongGia
+        {
+            set { this.donggia = value; }
+            get { return donggia; }
+        }
+        public string Tostring()
+        {
+            return string.Format("{0} {1} {2} {3} {4}", macd, tuacd, casi, sobaihat, donggia);
+        }
+    }
+}
---
+++ baitap5/QuanlyCD.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap5
+{
+    class QuanlyCD
+    {
+        private CD[] ds;
+        private int n;
+
+        public QuanlyCD()
+        {
+            ds = new CD[100];
+            n = 0;
+        }
+        public QuanlyCD(int sophantu)
+        {
+            ds = new CD[sophantu];
+            n = 0;
+        }
+        public void ThemCD(CD cd)
+        {
+            if (n >= ds.Length)
+            {
+                Console.WriteLine("danh sach da day.");
+            }
+            else
+            {
+                if (!kiemTraTrungCD(cd.MACD))
+                    ds[n++] = cd;
+                else
+                {
+                    Console.WriteLine("Trung ma CD");
+
+                }
+            }
+           
+        }
+        private bool kiemTraTrungCD(int macd)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (ds[i].MACD == macd)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public double tinhGiaTB()
+        {
+            int tonggia = 0;
+            for (int i = 0; i < n; i++)
+            {
+                tonggia += ds[i].DongGia;
+            }
+            return (double)tonggia / n;
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("{0, 10} {1,30} {2, 30} {3, 10} {4,15}",
+                "MaCD", "Tua CD", "Ca si", "So bai hat", "Gia thanh");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(ds[i].Tostring());
+            }
+        }
+        public void SapxepGiamThemGiaThanh()
+        {
+            CD tam;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (ds[i].DongGia < ds[i].DongGia)
+                    {
+                        tam = ds[i];
+                        ds[i] = ds[j];
+                        ds[j] = tam;
+                    }
+                }
+            }
+        }
+    }
+}
